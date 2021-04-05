9d7369a5f6e4170d2d462b59b10f648049fe38fc
eadsa1998 <69226325+eadsa1998@users.noreply.github.com>
2021-04-04 19:52:22 -0500

    Updated megatable and load statements
    Data files too big to upload here, will email them later

--- project2_part2.sql
+++ project2_part2.sql
@@ -2,9 +2,19 @@
 DROP DATABASE IF EXISTS election_tweets;
 CREATE DATABASE IF NOT EXISTS election_tweets ;
 
+-- Other potential columns:
+-- entry_id (each csv file has a tweet_id but if we combine them we'll need a new unique ID)
+-- candidate (to keep track of which dataset they came from, could easily be added as 0/1 so we can get counts later)
+-- like/retweet ratio?? (not sure it would really be useful but could end up being so)
+
+
+
+
+
+
 -- Create Megatable Statement
-DROP TABLE IF EXISTS tweet_megatable;
-CREATE TABLE IF NOT EXISTS tweet_megatable (
+DROP TABLE IF EXISTS biden_tweet_megatable;
+CREATE TABLE IF NOT EXISTS biden_tweet_megatable (
 created_at VARCHAR(255) DEFAULT NULL,
 tweet_id VARCHAR(255) DEFAULT NULL,
 tweet VARCHAR(8000) DEFAULT NULL,
@@ -17,27 +27,52 @@ user_screen_name VARCHAR(255) DEFAULT NULL,
 user_description VARCHAR(8000) DEFAULT NULL,
 user_join_date VARCHAR(255) DEFAULT NULL,
 user_followers_count VARCHAR(255) DEFAULT NULL,
-user_location VARCHAR(255) DEFAULT NULL, 
+user_location VARCHAR(8000) DEFAULT NULL, 
 lat VARCHAR(255) DEFAULT NULL,
 lon VARCHAR(255) DEFAULT NULL,
 city VARCHAR(255) DEFAULT NULL,
 country VARCHAR(255) DEFAULT NULL,
 continent VARCHAR(255) DEFAULT NULL,
 state VARCHAR(255) DEFAULT NULL,
-state_code CHAR(10) DEFAULT NULL,
+state_code VARCHAR(255) DEFAULT NULL,
 collected_at VARCHAR(255) DEFAULT NULL
 ) ENGINE = INNODB;
 
 
 LOAD DATA INFILE 'C:\\wamp64\\tmp\\hashtag_joebiden.csv'
-INTO TABLE tweet_megatable
+INTO TABLE biden_tweet_megatable
 FIELDS TERMINATED BY ','
 	   ENCLOSED BY '"'
        ESCAPED BY '\\'
 IGNORE 1 LINES;
 
+DROP TABLE IF EXISTS trump_tweet_megatable;
+CREATE TABLE IF NOT EXISTS trump_tweet_megatable (
+created_at VARCHAR(255) DEFAULT NULL,
+tweet_id VARCHAR(255) DEFAULT NULL,
+tweet VARCHAR(8000) DEFAULT NULL,
+likes VARCHAR(255) DEFAULT NULL,
+retweet_count VARCHAR(255) DEFAULT NULL,
+source VARCHAR(255) DEFAULT NULL,
+user_id VARCHAR(255) DEFAULT NULL,
+user_name VARCHAR(8000) DEFAULT NULL,
+user_screen_name VARCHAR(255) DEFAULT NULL,
+user_description VARCHAR(8000) DEFAULT NULL,
+user_join_date VARCHAR(255) DEFAULT NULL,
+user_followers_count VARCHAR(255) DEFAULT NULL,
+user_location VARCHAR(8000) DEFAULT NULL, 
+lat VARCHAR(255) DEFAULT NULL,
+lon VARCHAR(255) DEFAULT NULL,
+city VARCHAR(255) DEFAULT NULL,
+country VARCHAR(255) DEFAULT NULL,
+continent VARCHAR(255) DEFAULT NULL,
+state VARCHAR(255) DEFAULT NULL,
+state_code VARCHAR(255) DEFAULT NULL,
+collected_at VARCHAR(255) DEFAULT NULL
+) ENGINE = INNODB;
+
 LOAD DATA INFILE 'C:\\wamp64\\tmp\\hashtag_donaldtrump.csv'
-INTO TABLE tweet_megatable
+INTO TABLE trump_tweet_megatable
 FIELDS TERMINATED BY ','
 	   ENCLOSED BY '"'
        ESCAPED BY '\\'

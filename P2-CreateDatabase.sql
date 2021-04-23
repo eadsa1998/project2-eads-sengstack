56f847ae6b11459d6da405ccfef7e2f3e0a49762
Donnie Sengstack <69328568+donsengstack@users.noreply.github.com>
2021-04-22 23:37:41 -0500

    Add files via upload

---
+++ P2-CreateDatabase.sql
@@ -0,0 +1,148 @@
+-- Create Database Statement
+# First we need to create the database we are going to use for this project. The data base will be called election_tweets
+DROP DATABASE IF EXISTS election_tweets;
+CREATE DATABASE IF NOT EXISTS election_tweets ;
+
+USE election_tweets;
+
+-- Create table to load in Biden tweet data
+DROP TABLE IF EXISTS biden_tweet_loadintable;
+CREATE TABLE IF NOT EXISTS biden_tweet_loadintable (
+created_at VARCHAR(255) DEFAULT NULL,
+tweet_id VARCHAR(16) NOT NULL, # Changed to VARCHAR to load in all data
+tweet VARCHAR(8000) DEFAULT NULL,
+likes INT UNSIGNED NOT NULL, # Numeric value we want to perform math on
+retweet_count INT UNSIGNED NOT NULL, # Numeric value we want to perform math on
+source VARCHAR(255) DEFAULT NULL,
+user_id BIGINT UNSIGNED NOT NULL, # Made int beacuase will be a primary key in a decomposed table
+user_name VARCHAR(8000) DEFAULT NULL,
+user_screen_name VARCHAR(255) DEFAULT NULL,
+user_description VARCHAR(8000) DEFAULT NULL,
+user_join_date VARCHAR(255) DEFAULT NULL,
+user_followers_count INT UNSIGNED NOT NULL,
+user_location VARCHAR(8000) DEFAULT NULL, 
+lat VARCHAR(255) DEFAULT NULL,
+lon VARCHAR(255) DEFAULT NULL,
+city VARCHAR(255) DEFAULT NULL,
+country VARCHAR(255) DEFAULT NULL,
+continent VARCHAR(255) DEFAULT NULL,
+state VARCHAR(255) DEFAULT NULL,
+state_code VARCHAR(255) DEFAULT NULL,
+collected_at VARCHAR(255) DEFAULT NULL # Most varibles were made VARCHAR to ensure that we did not miss any data in loading
+) ENGINE = INNODB;
+
+# Load in the Biden tweet data 
+# The CSV file can be found at this link https://www.kaggle.com/manchunhui/us-election-2020-tweets?select=hashtag_joebiden.csv
+LOAD DATA INFILE 'C:\\wamp64\\tmp\\hashtag_joebiden.csv'
+IGNORE INTO TABLE biden_tweet_loadintable
+FIELDS TERMINATED BY ','
+	   ENCLOSED BY '"'
+       ESCAPED BY '\\'
+IGNORE 1 LINES;
+
+-- Create table to load in Trump tweet data
+DROP TABLE IF EXISTS trump_tweet_loadintable;
+CREATE TABLE IF NOT EXISTS trump_tweet_loadintable (
+created_at VARCHAR(255) DEFAULT NULL,
+tweet_id VARCHAR(16) NOT NULL, # Changed to VARCHAR to load in all data
+tweet VARCHAR(8000) DEFAULT NULL,
+likes INT UNSIGNED NOT NULL, # Numeric value we want to perform math on
+retweet_count INT UNSIGNED NOT NULL, # Numeric value we want to perform math on
+source VARCHAR(255) DEFAULT NULL,
+user_id BIGINT UNSIGNED NOT NULL, # Made int because we want it to be a primary key in a decomposed table
+user_name VARCHAR(8000) DEFAULT NULL,
+user_screen_name VARCHAR(255) DEFAULT NULL,
+user_description VARCHAR(8000) DEFAULT NULL,
+user_join_date VARCHAR(255) DEFAULT NULL,
+user_followers_count INT UNSIGNED DEFAULT NULL,
+user_location VARCHAR(8000) DEFAULT NULL, 
+lat VARCHAR(255) DEFAULT NULL,
+lon VARCHAR(255) DEFAULT NULL,
+city VARCHAR(255) DEFAULT NULL,
+country VARCHAR(255) DEFAULT NULL,
+continent VARCHAR(255) DEFAULT NULL,
+state VARCHAR(255) DEFAULT NULL,
+state_code VARCHAR(255) DEFAULT NULL,
+collected_at VARCHAR(255) DEFAULT NULL # Most varibles were made VARCHAR to ensure that we did not miss any data in loading
+) ENGINE = INNODB;
+
+# Load in the trump tweet data
+# The CSV file can be found at this link https://www.kaggle.com/manchunhui/us-election-2020-tweets?select=hashtag_donaldtrump.csv
+LOAD DATA INFILE 'C:\\wamp64\\tmp\\hashtag_donaldtrump.csv'
+IGNORE INTO TABLE trump_tweet_loadintable
+FIELDS TERMINATED BY ','
+	   ENCLOSED BY '"'
+       ESCAPED BY '\\'
+IGNORE 2 LINES;
+
+-- Create table to load in election result data
+DROP TABLE IF EXISTS election_results_megatable;
+CREATE TABLE IF NOT EXISTS election_results_megatable (
+state VARCHAR(255) DEFAULT NULL, # Made VARCHAR to ensure we could load all data
+county VARCHAR(255) DEFAULT NULL,
+candidate VARCHAR(8000) DEFAULT NULL,
+party VARCHAR(255) DEFAULT NULL,
+total_votes INT UNSIGNED, # Numeric variable we want to do math on
+won_race VARCHAR(255) DEFAULT NULL
+) ENGINE = INNODB;
+
+# Load in the election results data
+# The CSV file can be found at this link https://www.kaggle.com/unanimad/us-election-2020?select=president_county_candidate.csv
+LOAD DATA INFILE 'C:\\wamp64\\tmp\\president_county_candidate.csv'
+INTO TABLE election_results_megatable
+FIELDS TERMINATED BY ','
+	   ENCLOSED BY '"'
+       ESCAPED BY '\\'
+IGNORE 1 LINES;
+
+-- Create Mega table for all tweets
+DROP TABLE IF EXISTS tweet_megatable;
+CREATE TABLE IF NOT EXISTS tweet_megatable (
+created_at VARCHAR(255) DEFAULT NULL, # All types are the same as inital loading tables unless otherwise noted
+tweet_id VARCHAR(16) NOT NULL, # Changed to VARCHAR to load in all data,
+tweet VARCHAR(8000) DEFAULT NULL,
+likes VARCHAR(255) DEFAULT NULL,
+retweet_count VARCHAR(255) DEFAULT NULL,
+source VARCHAR(255) DEFAULT NULL,
+user_id BIGINT UNSIGNED NOT NULL,
+user_name VARCHAR(8000) DEFAULT NULL,
+user_screen_name VARCHAR(255) DEFAULT NULL,
+user_description VARCHAR(8000) DEFAULT NULL,
+user_join_date VARCHAR(255) DEFAULT NULL,
+user_followers_count VARCHAR (255) DEFAULT NULL,
+user_location VARCHAR(8000) DEFAULT NULL, 
+lat VARCHAR(255) DEFAULT NULL,
+lon VARCHAR(255) DEFAULT NULL,
+city VARCHAR(255) DEFAULT NULL,
+country VARCHAR(255) DEFAULT NULL,
+continent VARCHAR(255) DEFAULT NULL,
+state VARCHAR(255) DEFAULT NULL,
+state_code VARCHAR(255) DEFAULT NULL,
+collected_at VARCHAR(255) DEFAULT NULL,
+total_popularity INT UNSIGNED, # Numeric variable we want to do math on
+candidate VARCHAR (30),
+tweet_ratio FLOAT # This is a float because it will be a ratio with a decimal point
+);
+
+# Insert Biden info into the tweet mega table
+
+INSERT INTO tweet_megatable
+SELECT *, (likes + retweet_count) AS total_popularity, "Joe Biden" AS tweet_candidate, 
+CASE
+    WHEN retweet_count = 0 THEN likes/1 
+    ELSE likes/retweet_count
+END AS tweet_ratio
+FROM election_tweets.biden_tweet_loadintable;
+
+
+# Insert Trump info into the tweet mega table
+ 
+INSERT INTO tweet_megatable
+SELECT created_at, tweet_id, tweet, likes, retweet_count, source, user_id, user_name, user_screen_name, user_description, user_join_date, user_followers_count, user_location, lat, lon, city, country, continent, state, state_code, collected_at, (likes + retweet_count) AS total_popularity, "Donald Trump" AS tweet_candidate, 
+CASE
+    WHEN retweet_count = 0 THEN likes/1 
+    ELSE likes/retweet_count
+END AS tweet_ratio
+FROM election_tweets.trump_tweet_loadintable;
+
+
--- P2-NormalizedTables.sql
+++ P2-NormalizedTables.sql
@@ -1,10 +1,14 @@
 # In this SQL script we will create populate decomposed tables with info from the mega table.
+# We want to sued the election tweets data_base that we created in the mega tables file
 USE election_tweets;
 
+# All data types are the same as they were when loaded into the megatables
+
 # Create the table for each specific tweet
-DROP TABLE IF EXISTS tweet_info;
-CREATE TABLE IF NOT EXISTS tweet_info (
-tweet_id INT UNSIGNED AUTO_INCREMENT,
+DROP TABLE IF EXISTS tweet_info_load;
+CREATE TABLE IF NOT EXISTS tweet_info_load (
+entry_id INT UNSIGNED AUTO_INCREMENT,
+tweet_id VARCHAR (16),
 user_id BIGINT UNSIGNED NOT NULL,
 created_at VARCHAR(255) DEFAULT NULL,
 collected_at VARCHAR(255) DEFAULT NULL,
@@ -15,25 +19,60 @@ retweet_count INT UNSIGNED NOT NULL,
 total_popularity INT UNSIGNED, 
 candidate VARCHAR (30),
 tweet_ratio FLOAT,
-PRIMARY KEY (tweet_id),
-CONSTRAINT fk_user_info FOREIGN KEY (user_id)
+PRIMARY KEY (entry_id), # Set primary key to entry_id
+CONSTRAINT fk_user_info FOREIGN KEY (user_id) # User ID will be a foregin key in the user_info table
 		REFERENCES election_tweets.user_info (user_id)
         ON UPDATE CASCADE
 		ON DELETE NO ACTION,
-CONSTRAINT fk_user_location FOREIGN KEY (user_id)
+CONSTRAINT fk_user_location FOREIGN KEY (user_id) # User ID will be a foregin key in the user_info table
 		REFERENCES election_tweets.user_location (user_id)
         ON UPDATE CASCADE
 		ON DELETE NO ACTION,
-CONSTRAINT fk_candidate FOREIGN KEY (candidate)
+CONSTRAINT fk_candidate FOREIGN KEY (candidate) # Candidate will be a foregin key in the election_results table
 		REFERENCES election_tweets.election_results (candidate)
         ON UPDATE CASCADE
 		ON DELETE NO ACTION 
 );
 
-# Populate the tweet info table
-INSERT INTO tweet_info
+# Populate the tweet info table with data from the tweet megatable
+INSERT INTO tweet_info_load (tweet_id, user_id, created_at, collected_at, source, tweet, likes, retweet_count, total_popularity, candidate, tweet_ratio)
 SELECT tweet_id, user_id, created_at, collected_at, source, tweet, likes, retweet_count, total_popularity, candidate, tweet_ratio
 FROM election_tweets.tweet_megatable;
+
+# Create the table for each specific tweet where tweet_id is the primary_key
+DROP TABLE IF EXISTS tweet_info;
+CREATE TABLE IF NOT EXISTS tweet_info (
+tweet_id INT UNSIGNED,
+user_id BIGINT UNSIGNED NOT NULL,
+created_at VARCHAR(255) DEFAULT NULL,
+collected_at VARCHAR(255) DEFAULT NULL,
+source VARCHAR(255) DEFAULT NULL,
+tweet VARCHAR(8000) DEFAULT NULL,
+likes INT UNSIGNED NOT NULL,
+retweet_count INT UNSIGNED NOT NULL,
+total_popularity INT UNSIGNED, 
+candidate VARCHAR (30),
+tweet_ratio FLOAT,
+PRIMARY KEY (tweet_id), # Set primary key to tweet_id
+CONSTRAINT fk_user_info FOREIGN KEY (user_id) # User ID will be a foregin key in the user_info table
+		REFERENCES election_tweets.user_info (user_id)
+        ON UPDATE CASCADE
+		ON DELETE NO ACTION,
+CONSTRAINT fk_user_location FOREIGN KEY (user_id) # User ID will be a foregin key in the user_info table
+		REFERENCES election_tweets.user_location (user_id)
+        ON UPDATE CASCADE
+		ON DELETE NO ACTION,
+CONSTRAINT fk_candidate FOREIGN KEY (candidate) # Candidate will be a foregin key in the election_results table
+		REFERENCES election_tweets.election_results (candidate)
+        ON UPDATE CASCADE
+		ON DELETE NO ACTION 
+);
+
+# Populate the tweet info table with data from the tweet megatable
+INSERT INTO tweet_info
+SELECT entry_id, user_id, created_at, collected_at, source, tweet, likes, retweet_count, total_popularity, candidate, tweet_ratio
+FROM election_tweets.tweet_info_load;
+
 
 # Create the table for user info
 DROP TABLE IF EXISTS user_info;
@@ -44,16 +83,16 @@ user_screen_name VARCHAR(255) DEFAULT NULL,
 user_description VARCHAR(8000) DEFAULT NULL,
 user_join_date VARCHAR(255) DEFAULT NULL,
 user_followers_count VARCHAR (255) DEFAULT NULL,
-PRIMARY KEY (user_id)
+PRIMARY KEY (user_id) # Make user_id the primary key
 );
 
-# Populate the user info table
+# Populate the user info table with data from the tweet megatable
 INSERT INTO user_info
 SELECT user_id, user_name, user_screen_name, user_description, user_join_date, user_followers_count
 FROM election_tweets.tweet_megatable
 GROUP BY user_id;
 
-#Create the table for location info
+#Create the table for user location info
 DROP TABLE IF EXISTS user_location;
 CREATE TABLE IF NOT EXISTS user_location (
 user_id BIGINT UNSIGNED NOT NULL,
@@ -65,14 +104,14 @@ country VARCHAR(255) DEFAULT NULL,
 continent VARCHAR(255) DEFAULT NULL,
 state VARCHAR(255) DEFAULT NULL,
 state_code VARCHAR(255) DEFAULT NULL,
-PRIMARY KEY (user_id),
-CONSTRAINT fk_state FOREIGN KEY (state)
+PRIMARY KEY (user_id), # Make user_id the primary_key
+CONSTRAINT fk_state FOREIGN KEY (state) # Make state a foreign key in the election results table
 		REFERENCES election_tweets.election_results (state)
         ON UPDATE CASCADE
 		ON DELETE NO ACTION 
 );
 
-# Populate the user location table
+# Populate the user location table with data from the tweet megatable
 INSERT INTO user_location
 SELECT user_id, user_location, lat, lon, city, country, continent, state, state_code
 FROM election_tweets.tweet_megatable
@@ -86,8 +125,8 @@ county VARCHAR(30),
 candidate VARCHAR(30),
 total_votes INT UNSIGNED,
 won_race VARCHAR(30),
-PRIMARY KEY (state, county, candidate),
-CONSTRAINT fk_candidate_party FOREIGN KEY (candidate)
+PRIMARY KEY (state, county, candidate), # Make state, county, and candidate combined the primary key
+CONSTRAINT fk_candidate_party FOREIGN KEY (candidate) # Make candiate in the candidate_party table a foreign key
 		REFERENCES election_tweets.candidate_party (candidate)
         ON UPDATE CASCADE
 		ON DELETE NO ACTION 
@@ -98,16 +137,16 @@ DROP TABLE IF EXISTS candidate_party;
 CREATE TABLE IF NOT EXISTS candidate_party (
 candidate VARCHAR(30),
 party VARCHAR(5),
-PRIMARY KEY (candidate)
+PRIMARY KEY (candidate) # Make candidate the primary key
 );
 
-# Load data into election_results table
+# Load data relating to Trump and Biden only into election_results table from the election megatable
 INSERT INTO election_results
 SELECT state, county, candidate, total_votes, won_race
 FROM election_results_megatable
 WHERE candidate = "Joe Biden" OR candidate = "Donald Trump";
 
-# Load data into candidate party table
+# Load data relating to Trump and Biden only into candidate party table from the election megatable
 INSERT INTO candidate_party
 SELECT candidate, party 
 FROM election_results_megatable
